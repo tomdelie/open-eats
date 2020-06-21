3b4d919fefc8a0527480eceec0e96563300fb773
Tom Délié <tomdelie78@gmail.com>
2020-06-21 19:16:41 +0200

    Add unit tests config

---
+++ tests/User.spec.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+describe('User class', () => {
+
+  describe('true', () => {
+    it('should be true', () => {
+      expect(true).to.be.true;
+    });
+  });
+
+});
