85bfc3a049da3adbc03bbf2ec3e8c5b80b8eb005
Tom Délié <tomdelie78@gmail.com>
2020-06-25 18:46:07 +0200

    Add comment posting backend

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -4,11 +4,23 @@ const router = express.Router();
 
 router.get('/:id', async (req, res) => {
   const restaurant = await db.collection('restaurants').doc(req.params.id).get();
-
+  //const comments = restaurant.collection('comments').get();
   res.render('restaurants/restaurant', {
     restaurant: restaurant,
+    //comments: comments,
     session: firebase.auth().currentUser
   });
+});
+
+router.post('/:id/commentaire', async (req, res) => {
+  const { rating, comment } = req.body;
+  console.log(req.body)
+  db.collection('restaurants').doc(req.params.id).collection('comments').doc().set({
+    comment: comment,
+    rating: rating
+  });
+  const restaurant = await db.collection('restaurants').doc(req.params.id).get();
+  res.redirect(`/restaurants/${id}`);
 });
 
 module.exports = router;
