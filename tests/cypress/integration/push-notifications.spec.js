5489bdf16c42db1c50ff6d4b92d7ef4eb8934f90
Tom Délié <tomdelie78@gmail.com>
2020-06-30 19:20:20 +0200

    Fix cypress CI

--- tests/cypress/integration/push-notifications.spec.js
+++ tests/cypress/integration/push-notifications.spec.js
@@ -1,28 +1,25 @@
 describe('Push Notifications', () => {
     it('Granted', () => {
-        cy.visit('http://localhost:3000');
-        cy.clearLocalStorage();
-        cy.get('.notifications[data-status="granted"]').click({ force: true }).should(() => {
+        cy.visit('http://localhost:3000').should(() => {
+            cy.get('.notifications[data-status="granted"]').click({ force: true });
             expect(localStorage.getItem('notifications')).to.eq('granted');
+            cy.get('#permission-icon').should('have.attr', 'src').and('eq', '/images/notifications_active.svg');
         });
-        cy.get('#permission-icon').should('have.attr', 'src').and('eq', '/images/notifications_active.svg');
     });
 
     it('Default', () => {
-        cy.visit('http://localhost:3000');
-        cy.clearLocalStorage();
-        cy.get('.notifications[data-status="default"]').click({ force: true }).should(() => {
+        cy.visit('http://localhost:3000').should(() => {
+            cy.get('.notifications[data-status="default"]').click({ force: true });
             expect(localStorage.getItem('notifications')).to.eq('default');
+            cy.get('#permission-icon').should('have.attr', 'src').and('eq', '/images/notifications.svg');
         });
-        cy.get('#permission-icon').should('have.attr', 'src').and('eq', '/images/notifications.svg');
     });
 
     it('Denied', () => {
-        cy.visit('http://localhost:3000');
-        cy.clearLocalStorage();
-        cy.get('.notifications[data-status="denied"]').click({ force: true }).should(() => {
+        cy.visit('http://localhost:3000').should(() => {
+            cy.get('.notifications[data-status="denied"]').click({ force: true })
             expect(localStorage.getItem('notifications')).to.eq('denied');
+            cy.get('#permission-icon').should('have.attr', 'src').and('eq', '/images/notifications_off.svg');
         });
-        cy.get('#permission-icon').should('have.attr', 'src').and('eq', '/images/notifications_off.svg');
     });
 });
