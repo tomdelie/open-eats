dc74381949239fc5ba5be837ead4fb23e1f3caab
Tom Délié <tomdelie78@gmail.com>
2020-07-06 17:30:47 +0200

    Refacto firebase directory

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')({origin: true});
 
 const indexRouter = require('./routes/index');
 const { router: securityRouter } = require('./routes/security');
@@ -16,6 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
